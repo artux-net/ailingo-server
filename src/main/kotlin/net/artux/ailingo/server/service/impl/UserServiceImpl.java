4c6db1067cc788107b2f8038bac043ac3a11d311
Artemka228not <q2w34132@gmail.com>
2024-11-23 19:45:03 +0300

    user streak 24

--- src/main/kotlin/net/artux/ailingo/server/service/impl/UserServiceImpl.java
+++ src/main/kotlin/net/artux/ailingo/server/service/impl/UserServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
