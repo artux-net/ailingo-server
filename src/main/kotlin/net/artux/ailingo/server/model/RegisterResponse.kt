246cd10d1dba281a9617a363fc826739ec1bb94a
Maxim <maksim.prygunov@gmail.com>
2025-02-19 21:46:01 +0400

    OpenAI integration update

--- src/main/kotlin/net/artux/ailingo/server/model/RegisterResponse.kt
+++
@@ -1,9 +0,0 @@
-package net.artux.ailingo.server.model
-
-import net.artux.ailingo.server.dto.UserDto
-
-data class RegisterResponse(
-    val token: String,
-    val refreshToken: String,
-    val user: UserDto?
-)
