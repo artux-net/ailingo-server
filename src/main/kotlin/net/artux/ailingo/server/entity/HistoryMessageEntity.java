9a4fd9fede1c080f2d0f28374d7e4106f3a593d5
vangel <vangelnum@gmail.com>
2025-03-21 23:33:43 +0400

    chat fix

--- src/main/kotlin/net/artux/ailingo/server/entity/HistoryMessageEntity.java
+++ src/main/kotlin/net/artux/ailingo/server/entity/HistoryMessageEntity.java
@@ -2,6 +2,9 @@ package net.artux.ailingo.server.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -15,7 +18,13 @@ import java.util.UUID;
 @Setter
 @Getter
 @Table(name = "message_history")
-public class HistoryMessageEntity extends BaseEntity {
+public class HistoryMessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGSERIAL")
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "conversation_owner", nullable = false)
     private UserEntity owner;
