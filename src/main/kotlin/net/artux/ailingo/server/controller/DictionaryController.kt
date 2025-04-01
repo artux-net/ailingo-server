78201ca8199ad2a7b65eea80f76ef9b5ceaae983
Egor <laiven937@mail.ru>
2025-04-01 22:17:06 +0400

    Исправления по запросу в dictionary

--- src/main/kotlin/net/artux/ailingo/server/controller/DictionaryController.kt
+++ src/main/kotlin/net/artux/ailingo/server/controller/DictionaryController.kt
@@ -5,10 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import net.artux.ailingo.server.model.DictionaryResponse
 import net.artux.ailingo.server.service.DictionaryService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Словарь")
 @RestController
@@ -18,8 +15,8 @@ class DictionaryController(
 ) {
 
     @Operation(summary = "Получение описания введенного слова")
-    @GetMapping("/define/{word}")
-    fun getDefinition(@PathVariable word: String): ResponseEntity<List<DictionaryResponse>> {
+    @GetMapping("/define")
+    fun defineWord(@RequestParam word: String): ResponseEntity<List<DictionaryResponse>> {
         val definitions = dictionaryService.getWordDefinition(word)
         return ResponseEntity.ok(definitions)
     }
--- src/main/kotlin/net/artux/ailingo/server/model/DictionaryResponse.kt
+++ src/main/kotlin/net/artux/ailingo/server/model/DictionaryResponse.kt
@@ -5,20 +5,3 @@ data class DictionaryResponse(
     val phonetics: List<Phonetic>,
     val meanings: List<Meaning>
 )
-
-data class Phonetic(
-    val text: String,
-    val audio: String
-)
-
-data class Meaning(
-    val partOfSpeech: String,
-    val definitions: List<Definition>
-)
-
-data class Definition(
-    val definition: String,
-    val synonyms: List<String>,
-    val antonyms: List<String>,
-    val example: String?
-)
